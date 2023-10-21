1a1329055dec9224d9c0040cd6e297153c0ac6ab
Erick Nunes De Araujo <enaraujo@stefanini.com>
2023-10-21 19:27:05 -0300

    @erickNA2 ajustes

--- site/public/js/recover.js
+++ site/public/js/recover.js
@@ -1,6 +1,7 @@
-const nodemailer = require("nodemailer");
-
 const recoverDiv = document.getElementById("recover");
+
+const serviceID = "";
+const templateID = "";
 
 function gerarCodigo() {
 	let codigo = "";
@@ -10,53 +11,40 @@ function gerarCodigo() {
 	for (let i = 1; i <= 6; i++) {
 		let char = Math.floor(Math.random() * caracteres.length + 1);
 
-		codigo += str.charAt(char);
+		codigo += caracteres.charAt(char);
 	}
 
 	return codigo;
 }
+
+(function(){
+	emailjs.init("gjl6RaM3oy6VIOIWcz");
+ })();
+
+let codigoRecuperacao = gerarCodigo();
 
 
 function enviarCodigo() {
 	let email = document.getElementById("email").value;
 
 	if (email != "") {
-		("use strict");
-		const nodemailer = require("nodemailer");
 
-		const transporter = nodemailer.createTransport({
-			host: "outlook.com",
-			port: 465,
-			secure: true,
-			auth: {
-				// TODO: replace `user` and `pass` values from <https://forwardemail.net>
-				user: "erick.araujo@sptech.school",
-				pass: "#Gf51786458802",
-			},
-		});
-
-		// async..await is not allowed in global scope, must use a wrapper
-		async function main() {
-			// send mail with defined transport object
-			const info = await transporter.sendMail({
-				from: '"Fred Foo 👻" <erick.araujo@sptech.school>', // sender address
-				to: `${email}`, // list of receivers
-				subject: "Codigo de autenticação", // Subject line
-				text: `seu codigo é ${gerarCodigo()}`, // plain text body
-				html: "<b>Hello world?</b>", // html body
-			});
-
-			console.log("Message sent: %s", info.messageId);
-			// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-			//
-			// NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-			//       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-			//       <https://github.com/forwardemail/preview-email>
-			//
+		function sendMail() {
+			var params = {
+				name: "me",
+				email: "araujoerick.n1@gmail.com",
+				message: `Seu codigo de verificação é ${codigoRecuperacao}.`
+			}
 		}
 
-		main().catch(console.error);
+		emailjs.send(serviceID, templateID, params).then(
+			res => {
+				console.log("success!")
+				console.log(res)
+			}
+		).catch((err) => console.log(err))
+
+		sendMail()
 
 		recoverDiv.innerHTML = `
         <div class="box">
@@ -72,5 +60,17 @@ function enviarCodigo() {
                 Enviar
             </button>
         `;
+	} else {
+		alert("Preencha o email.")
+	}
+}
+
+function validarCodigo() {
+	let validador = document.getElementById("validadorCodigo")
+
+	if (validador == codigoRecuperacao) {
+		alert("verificado!")
+	} else {
+		alert("codigo invalido!")
 	}
 }
