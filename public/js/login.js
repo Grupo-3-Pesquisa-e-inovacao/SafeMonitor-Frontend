270e0fba9fc7d13b84932b8d8d7a7369de74416a
Douglas Queiroz <douglas.queiroz@sptech.school>
2023-11-13 20:19:33 -0300

    Iniciando a conexão

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,51 @@
 
-
-let listaUsuarios = 
-[{usuario: 'alexandre',
-senha: 29932},
-{usuario: 'felipe',
-senha: 83392}, 
-{usuario: 'vinicius',
-senha: 99283}]
-
 function validarLogin() {
-    let usuario = document.getElementById('email').value
-    let senha = document.getElementById('pass').value
+    let emailVar = document.getElementById('email').value
+    let senhaVar = document.getElementById('pass').value
 
-    for(let i = 0; i < listaUsuarios.length; i++) {
-        if(listaUsuarios[i].usuario == usuario && listaUsuarios[i].senha == senha) {
-            alert("login bem sussedido")
-            return
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            emailServer: emailVar,
+            senhaServer: senhaVar
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()!")
+
+        if (resposta.ok) {
+            console.log(resposta);
+
+            resposta.json().then(json => {
+                console.log(json);
+                console.log(JSON.stringify(json));
+                sessionStorage.EMAIL_USUARIO = json.email;
+                sessionStorage.NOME_USUARIO = json.nome;
+                sessionStorage.ID_USUARIO = json.idUsuario;
+                sessionStorage.ID_EMPRESA = json.fk_empresa;
+            
+
+                // setTimeout(function () {
+                //     window.location = "./dashboard/cards.html";
+                // }, 1000); // apenas para exibir o loading
+
+            });
+
         } else {
-            alert("Login ou senha incorretos.") 
+
+            console.log("Houve um erro ao tentar realizar o login!");
+
+            // resposta.text().then(texto => {
+            //     console.error(texto);
+            //     finalizarAguardar(texto);
+            // });
         }
-    }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
 }
 
 function acessarLogin() {
